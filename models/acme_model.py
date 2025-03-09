3b67e6f94ff01ac9ed007e23faf9e7e1378c8a70
sabughazal <sultan.abughazal@tii.ae>
2025-03-10 03:53:23 +0400

    Update acme_model.py

--- models/acme_model.py
+++ models/acme_model.py
@@ -1,8 +1,19 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 class AcmeModel(nn.Module):
     def __init__(self, input_size, num_classes):
-        pass
+        super().__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(input_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 128),
+            nn.ReLU(),
+            nn.Linear(128, 32),
+            nn.ReLU(),
+            nn.Linear(32, num_classes),
+        )
 
     def forward(self, x):
-        pass
+        x = self.fc(x)
+        return x, F.softmax(x, dim=1)
